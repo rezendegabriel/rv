d1b0ce152548d6d096b25e118acf8e74c50c6ab1
rezendegabriel <rezende.gabriel@engenharia.ufjf.br>
2022-06-29 21:03:06 -0300

    gt partial exercises

--- exercises/gtExercise01.js
+++ exercises/gtExercise01.js
@@ -1,18 +1,18 @@
 import * as THREE from  'three';
 import {OrbitControls} from '../build/jsm/controls/OrbitControls.js';
-import {initRenderer,
+import {initRenderer, 
         initCamera,
         initDefaultBasicLight,
         setDefaultMaterial,
         InfoBox,
         onWindowResize,
-        createGroundPlaneXZ } from "../libs/util/util.js";
+        createGroundPlaneXZ} from "../libs/util/util.js";
 
 let scene, renderer, camera, material, light, orbit; // Initial variables
-scene = new THREE.Scene(); // Create main scene
-renderer = initRenderer(); // Init a basic renderer
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
 camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
-material = setDefaultMaterial("orange"); // Creates basic materials
+material = setDefaultMaterial(); // Create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
 
@@ -27,24 +27,46 @@ scene.add(axesHelper);
 let plane = createGroundPlaneXZ(20, 20)
 scene.add(plane);
 
-// Create cubes
-let dimCube, step, x, z, dx, dz, r, b;
-dimCube = 4;
-step = 2;
-x = z = -step*dimCube; // Initial position
-r = 3;
-b = 3;
+// Create the table
+let dimCube, g;
+dimCube = 1;
+g = 3;
+let cubeGeometry = new THREE.BoxGeometry(dimCube, dimCube, dimCube);
+let cube = new THREE.Mesh(cubeGeometry, material);
+
+cube.position.set(0.0, g, 0.0); // Position the cube
+cube.scale.set(11, 0.3, 6); // Set scale to create a table
+scene.add(cube); // Add the table to the scene
+
+// Creates the supports
+let radiusTop, radiusBottom, height, radialSegments, x, z, stepR, stepB, r, b, dx, dz;
+radiusTop = 0.2;
+radiusBottom = 0.2;
+height = 3;
+radialSegments = 100;
+
+// Initial positions
+x = -(11/2)+2*radiusTop;
+z = -(6/2)+2*radiusTop;
+
+stepR = -2*x;
+stepB = -2*z;
+r = 2;
+b = 2;
+
 for (var i = 0; i < r; i++) {
   for (var k = 0; k < b; k++) {
-    let cubeGeometry = new THREE.BoxGeometry(dimCube, dimCube, dimCube);
-    let cube = new THREE.Mesh(cubeGeometry, material);
+    let cylinderGeometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
+    let cylinder = new THREE.Mesh(cylinderGeometry, material);
   
-    // Cube position
-    dx = i*step*dimCube;
-    dz = k*step*dimCube;
-    cube.position.set(x+dx, dimCube/2, z+dz); // RGB
-    // Add the cube to the scene
-    scene.add(cube);
+    // Cylinder position
+    dx = i*stepR*dimCube;
+    dz = k*stepB*dimCube;
+    cylinder.position.set(x, height/2, z); // RGB
+    cylinder.translateX(dx);
+    cylinder.translateZ(dz);
+    // Add the cylinder to the scene
+    scene.add(cylinder);
   }
 }
 
@@ -59,7 +81,8 @@ let controls = new InfoBox();
   controls.show();
 
 render();
-function render() {
+function render()
+{
   requestAnimationFrame(render);
   renderer.render(scene, camera) // Render scene
 }
--- exercises/gtExercise02.js
+++ exercises/gtExercise02.js
@@ -1,18 +1,19 @@
 import * as THREE from  'three';
 import {OrbitControls} from '../build/jsm/controls/OrbitControls.js';
-import {initRenderer,
+import {initRenderer, 
         initCamera,
         initDefaultBasicLight,
         setDefaultMaterial,
         InfoBox,
         onWindowResize,
-        createGroundPlaneXZ } from "../libs/util/util.js";
+        degreesToRadians,
+        createGroundPlaneXZ} from "../libs/util/util.js";
 
 let scene, renderer, camera, material, light, orbit; // Initial variables
-scene = new THREE.Scene(); // Create main scene
-renderer = initRenderer(); // Init a basic renderer
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
 camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
-material = setDefaultMaterial("orange"); // Creates basic materials
+material = setDefaultMaterial(); // Create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
 
@@ -27,25 +28,39 @@ scene.add(axesHelper);
 let plane = createGroundPlaneXZ(20, 20)
 scene.add(plane);
 
-// Create cubes
-let dimCube, step, x, z, dx, dz, r, b;
-dimCube = 4;
-step = 2;
-x = z = -step*dimCube; // Initial position
-r = 3;
-b = 3;
-for (var i = 0; i < r; i++) {
-  for (var k = 0; k < b; k++) {
-    let cubeGeometry = new THREE.BoxGeometry(dimCube, dimCube, dimCube);
-    let cube = new THREE.Mesh(cubeGeometry, material);
-  
-    // Cube position
-    dx = i*step*dimCube;
-    dz = k*step*dimCube;
-    cube.position.set(x+dx, dimCube/2, z+dz); // RGB
-    // Add the cube to the scene
-    scene.add(cube);
-  }
+// Creates the spheres
+let radius, widthSegments, heightSegments, yStepAngle, x, z, numSpheres;
+radius = 0.5;
+widthSegments = 100;
+heightSegments = 100;
+
+yStepAngle = degreesToRadians(30);
+x = 8;
+z = 0;
+numSpheres = 12;
+
+for (var i = 0; i < numSpheres; i++) {
+  var sphere = createSphere(radius, widthSegments, heightSegments, material);
+
+  // Add the cylinder to the scene
+  scene.add(sphere);
+
+  sphere.matrixAutoUpdate = false;
+  sphere.matrix.identity(); // Resetting matrice
+
+  var mat4 = new THREE.Matrix4(); // Auxiliar matrix
+
+  // Translation and Rotation
+  sphere.matrix.multiply(mat4.makeRotationY(yStepAngle*i));
+  sphere.matrix.multiply(mat4.makeTranslation(x, radius, z));
+}
+
+function createSphere(radius, widthSegments, heightSegments, material)
+{
+  var sphereGeometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+  var sphere = new THREE.Mesh(sphereGeometry, material);
+
+  return sphere;
 }
 
 // Use this to show information onscreen
@@ -59,7 +74,9 @@ let controls = new InfoBox();
   controls.show();
 
 render();
-function render() {
+
+function render()
+{
   requestAnimationFrame(render);
   renderer.render(scene, camera) // Render scene
 }
