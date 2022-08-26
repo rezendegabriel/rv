6c0fde4c2affec2e0a9582e4f261f57be79549d5
rezendegabriel <rezende.gabriel@engenharia.ufjf.br>
2022-08-26 20:52:34 -0300

    updates main.js webserver.py

--- tf/main.js
+++ tf/main.js
@@ -104,13 +104,13 @@ function getWebSocketServer() {
 	  throw new Error(`Unsupported host: ${window.location.host}`);
   }
 
-function receivedLightPos() {
+function sendImg() {
 	const ws = new WebSocket(getWebSocketServer());
 		console.log(ws);
 
 	ws.onmessage = function(message) {
 		strPosLight = message.data;
-		console.log("[Light position received from the web server]");
+		console.log("[Msg received from the web server] ", strPosLight);
 
 		var paramsText2 = {showStrPosLight: strPosLight};
 		text2 = text1Folder.add(paramsText2, "showStrPosLight").name("");
@@ -120,30 +120,11 @@ function receivedLightPos() {
 	};
 
 	try {
-		ws.onopen = () => ws.send("[Interface reconnected to the web server]"); // Communication restablished with the web server
-			console.log("[Reconnected to the web server]");
-	} catch (error) {
-		console.log("[Not reconnected to the web server] ", error);
-	}
-}
-
-function sendImg() {
-	const ws = new WebSocket(getWebSocketServer());
-		console.log(ws);
-	
-	try {
 		ws.onopen = () => ws.send(imgDataURL); // Communication established with the web server
-			console.log("[Image URL sent to the web server]");
+			console.log("[Msg sent to the web server] ", imgDataURL);
 	} catch (error) {
-		console.log("[Image URL not sent to the web server] ", error);
+		console.log("[Not Connected to the web server] ", error);
 	}
-
-	setTimeout(() => {console.log("Wait 3 seconds..."); }, 3000);
-
-	ws.close()
-
-	var paramsButton3 = {onClick: receivedLightPos};
-	button3 = text1Folder.add(paramsButton3, "onClick").name("Receive Light Position");
 }
 
 //-- Capture button function ---------------------------------------------------------------------
@@ -173,7 +154,6 @@ var text1Folder = null;
 var text1 = null;
 var text2 = null;
 var button2 = null;
-var button3 = null;
 
 var button1Event = false;
 
