3a77ab53ace5fabeee66365feaff9924d321ede6
rezendegabriel <rezende.gabriel@engenharia.ufjf.br>
2022-08-31 11:21:31 -0300

    updates server.py

--- tf/server.py
+++ tf/server.py
@@ -8,41 +8,30 @@ CONNECTIONS = []
 IMG_URL = ""
 LIGHT_POS = ""
 
-IMG_URL_RECEIVED = False
-IMG_URL_SEND = False
-
-LIGHT_POS_SEND = False
-LIGHT_POS_RECEIVED = False
-    
 async def webserver(ws):
     global CONNECTIONS
     global IMG_URL, LIGHT_POS
-    global IMG_URL_RECEIVED, IMG_URL_SEND, LIGHT_POS_SEND, LIGHT_POS_RECEIVED
 
     CONNECTIONS.append(ws)
 
-    if not IMG_URL_RECEIVED:
+    if len(CONNECTIONS) == 1:
         IMG_URL = await CONNECTIONS[0].recv() # Received image URL from the interface
         print("[Msg received from the iterface] ", IMG_URL)
 
-        IMG_URL_RECEIVED = True
+        while LIGHT_POS == "":
+            if LIGHT_POS != "":
+                break
+
+        await CONNECTIONS[0].send(LIGHT_POS) # Sent light position to the interface
 
     if len(CONNECTIONS) == 2:
-        if IMG_URL_RECEIVED:
-            await CONNECTIONS[1].send(IMG_URL) # Sent image URL to the render
+        await CONNECTIONS[1].send(IMG_URL) # Sent image URL to the render
 
-            IMG_URL_SEND = True
+        LIGHT_POS = await CONNECTIONS[1].recv() # Received light position from the render
+        print("[Msg received from the render] ", LIGHT_POS)
 
-        if IMG_URL_SEND:
-            LIGHT_POS = await CONNECTIONS[1].recv() # Received light position from the render
-            print("[Msg received from the render] ", LIGHT_POS)
-
-            LIGHT_POS_RECEIVED = True
-
-        if LIGHT_POS_RECEIVED:
-            await CONNECTIONS[0].send(LIGHT_POS) # Sent light position to the interface
-
-            LIGHT_POS_SEND = True
+        CONNECTIONS.pop()
+    
 async def main():
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
